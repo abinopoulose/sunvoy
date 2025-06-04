b883677ea2e94418068584abc1f662f2c3749b8f
Abino Poulose <abinopoulose@outlook.com>
2025-06-05 01:05:16 +0530

    fix: sanitize user data before sending

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { UserInterface } from '../interfaces/user.interface';
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -6,9 +7,9 @@ export const userController = async (req: Request, res: Response) => {
     try {
         const usersPath = path.join(__dirname, '../constant/users.json');
         const usersData = await fs.readFile(usersPath, 'utf8');
-        const users = JSON.parse(usersData);
-        
-        res.json(users);
+        const userList = JSON.parse(usersData) as UserInterface[];
+        const sanitizedUserList = userList.map(({ password, ...user }) => user);
+        res.json(sanitizedUserList);
     } catch (error) {
         res.status(500).json({message: 'Error fetching users'});
     }
